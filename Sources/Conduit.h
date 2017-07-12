2517812d0530e55bdc31e4e3cfeb7d4f6de5dc14
John Hammerlund <johnhammerlund@gmail.com>
2017-07-12 09:11:22 -0700

    Add initial project and update .gitignore

---
+++ Sources/Conduit.h
@@ -0,0 +1,19 @@
+//
+//  Conduit.h
+//  Conduit
+//
+//  Created by John Hammerlund on 7/12/17.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Conduit.
+FOUNDATION_EXPORT double ConduitVersionNumber;
+
+//! Project version string for Conduit.
+FOUNDATION_EXPORT const unsigned char ConduitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Conduit/PublicHeader.h>
+
+
