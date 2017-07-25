66a52e1658ae99a2917852b99e290005068944c9
John Hammerlund <johnhammerlund@gmail.com>
2017-07-25 16:48:44 -0700

    Cocoa platforms need Foundation, too

--- Sources/Conduit/LinuxBridging.swift
+++ Sources/Conduit/LinuxBridging.swift
@@ -5,9 +5,9 @@
 //  Created by John Hammerlund on 7/21/17.
 //  Copyright © 2017 MINDBODY. All rights reserved.
 //
-#if os(Linux)
-
 import Foundation
+
+#if os(Linux)
 
 func arc4random_uniform(_ upperBound: UInt32) -> UInt32 {
     srandom(UInt32(time(nil)))
