b878a5be06903782c1eb2d362767f1afca391862
John Hammerlund <johnhammerlund@gmail.com>
2017-09-19 13:13:33 -0700

    (Bugfix)|Fix iOS 11 keychain access from test targets
    Without a test host, unit test targets for iOS and tvOS complain about missing entitlements starting with iOS 11. Running tests within a host application inside the simulator ignores any need for entitlements since simulator builds cannot be signed in the first place.

---
+++ TestHosts/tvOS/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ConduitTestHost-tvOS
+//
+//  Created by John Hammerlund on 9/19/17.
+//  Copyright © 2017 MINDBODY. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
