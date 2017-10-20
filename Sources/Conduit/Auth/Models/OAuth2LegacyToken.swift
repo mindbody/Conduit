693dcfc09c3d2a1ee26c0edbda66f6c9964c4bd0
John Hammerlund <johnhammerlund@gmail.com>
2017-10-19 17:27:51 -0700

    Remove debug code

--- Sources/Conduit/Auth/Models/OAuth2LegacyToken.swift
+++ Sources/Conduit/Auth/Models/OAuth2LegacyToken.swift
@@ -8,14 +8,10 @@
 
 import Foundation
 
-public protocol LegacyConvertible {
-    var converted: BearerToken { get }
-}
-
 /// A token issued from an OAuth2 server application that represents
 /// a possession factor (hence "bearer") for a specific user
 @available(*, deprecated, message: "NSObject subclasses are being removed; use BearerToken instead.")
-public class BearerOAuth2Token: NSObject, NSCoding, DataConvertible, OAuth2Token, LegacyConvertible {
+public class BearerOAuth2Token: NSObject, NSCoding, DataConvertible, OAuth2Token {
 
     /// The access token
     public let accessToken: String
