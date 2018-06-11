3676dd1c4fdcb635dd208d6cb3cd5713c9d286f7
John Hammerlund <johnhammerlund@gmail.com>
2018-06-11 13:58:05 -0700

    (Maint)|Add tests for XMLRequestSerializer and Auth.Migrator
    Adds coverage for XMLRequestSerializer and adds remaining coverage for Auth.Migrator.

---
+++ Tests/ConduitTests/Networking/Serialization/XML/XMLRequestSerializerTests.swift
@@ -0,0 +1,82 @@
+//
+//  XMLRequestSerializerTests.swift
+//  Conduit
+//
+//  Created by John Hammerlund on 6/11/18.
+//  Copyright © 2018 MINDBODY. All rights reserved.
+//
+
+import XCTest
+@testable import Conduit
+
+class XMLRequestSerializerTests: XCTestCase {
+
+    let testXML = XMLNode(name: "xml", children: [
+        XMLNode(name: "clients", children: [
+            XMLNode(name: "client", children: [
+                XMLNode(name: "id", value: "client1"),
+                XMLNode(name: "name", value: "Bob"),
+                XMLNode(name: "clientonly", value: "Foo")
+            ])
+        ])
+    ])
+
+    private func makeRequest() throws -> URLRequest {
+        let url = try URL(absoluteString: "http://localhost:3333")
+        var request = URLRequest(url: url)
+        request.httpMethod = "POST"
+        return request
+    }
+
+    func testSerializesXML() throws {
+        let request = try makeRequest()
+        let serializer = XMLRequestSerializer()
+
+        guard let modifiedRequest = try? serializer.serialize(request: request, bodyParameters: XML(root: testXML)) else {
+            XCTFail("Serialization failed")
+            return
+        }
+
+        guard let httpBody = modifiedRequest.httpBody,
+            let xmlString = String(data: httpBody, encoding: .utf8) else {
+            XCTFail("Expected body")
+            return
+        }
+
+        let xml = XML(xmlString)
+        XCTAssert(xml != nil)
+    }
+
+    func testConfiguresDefaultContentTypeHeader() throws {
+        let request = try makeRequest()
+        let serializer = XMLRequestSerializer()
+
+        guard let modifiedRequest = try? serializer.serialize(request: request, bodyParameters: nil) else {
+            XCTFail("Serialization failed")
+            return
+        }
+
+        XCTAssert(modifiedRequest.allHTTPHeaderFields?["Content-Type"] == "text/xml; charset=utf-8")
+    }
+
+    func testDoesntReplaceCustomDefinedHeaders() throws {
+        var request = try makeRequest()
+        let serializer = XMLRequestSerializer()
+
+        let customDefaultHeaderFields = [
+            "Accept-Language": "FlyingSpaghettiMonster",
+            "User-Agent": "Chromebook. Remember those?",
+            "Content-Type": "application/its-not-xml-but-it-kind-of-is-aka-soap"
+        ]
+        request.allHTTPHeaderFields = customDefaultHeaderFields
+
+        guard let modifiedRequest = try? serializer.serialize(request: request, bodyParameters: nil) else {
+            XCTFail("Serialization failed")
+            return
+        }
+        for customHeader in customDefaultHeaderFields {
+            XCTAssert(modifiedRequest.value(forHTTPHeaderField: customHeader.0) == customHeader.1)
+        }
+    }
+
+}
