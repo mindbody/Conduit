a77b32009c6b111dace4a8a9aad0cfa5393821af
Eneko Alonso <eneko.alonso@gmail.com>
2019-02-26 12:17:07 -0800

    OAuth2TokenAES256CBCCipher documentation

--- Sources/Conduit/Auth/TokenStorage/OAuth2TokenAES256CBCCipher.swift
+++ Sources/Conduit/Auth/TokenStorage/OAuth2TokenAES256CBCCipher.swift
@@ -8,19 +8,52 @@
 
 import Foundation
 
+/// AES-256 Cipher Block Chaining OAuth2 token dipher
+///
+/// Use in conjunction with `OAuth2TokenEncryptedStore` as follows:
+///
+///     let store = OAuth2TokenUserDefaultsStore()
+///     store.tokenCipher = OAuth2TokenAES256CBCCipher(passphrase: encryptionPassphrase)
+///
+/// A secure encryption key is generated from the given passphrase. The CBC IV is
+/// automatically generated and persisted together with the encrypted token.
+///
+/// Once the store token cipher has been set up, all stored tokens will be
+/// automatically encrypted by the store via `encrypt(token:)`.
+///
+///     store.store(token: myToken, ...) // automatic encryption
+///
+/// Retrieved tokens will be autimatically decrypted by the store via `decrypt(token:)`:
+///
+///     let token: BearerToken? = store.token(for: ...) // automatic decryption
+///
 public final class OAuth2TokenAES256CBCCipher: OAuth2TokenCipher {
 
     let cipher: AES256CBCCipher
 
+    /// Initialize token cipher with a give passphrase
+    ///
+    /// - Parameter passphrase: Passphrase used for encryption
+    /// - Throws: Exception if derivated key cannot be generated from passphrase
     public init(passphrase: String) throws {
         cipher = try AES256CBCCipher(passphrase: passphrase)
     }
 
+    /// Securely encrypt an `OAuth2Token` with AES-256 CBC
+    ///
+    /// - Parameter token: Token to encrypt
+    /// - Returns: Ciphertext data with encrypted token
+    /// - Throws: Exception if encryption failed
     public func encrypt<Token>(token: Token) throws -> Data where Token: DataConvertible, Token: OAuth2Token {
         let tokenData = try token.serialized()
         return try cipher.encrypt(data: tokenData)
     }
 
+    /// Securely decrypt AES-256 CBC encrypted tokens
+    ///
+    /// - Parameter cipherData: Ciphertext data containing the encrypted token
+    /// - Returns: Decrypted `OAuth2Token`
+    /// - Throws: Exception if decryption failed
     public func decrypt<Token>(data cipherData: Data) throws -> Token where Token: DataConvertible, Token: OAuth2Token {
         let data = try cipher.decrypt(data: cipherData)
         return try Token(serializedData: data)
